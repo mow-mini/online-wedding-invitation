bc6515224b528295ab4ab398ac3a04ed741167ab
Mow <yenhoang.it@ecombest.co>
2024-02-27 09:17:57 +0700

    Onclick

--- assets/script/card/index.js
+++ assets/script/card/index.js
@@ -1,11 +1,21 @@
+const LOAD_TIME = 1500;
+
 window.addEventListener("DOMContentLoaded", (event) => {
-  console.log(123);
+  const openCardBtn = document.getElementById("open-card-btn");
+  const cardCover = document.getElementById("card-cover");
+  if (openCardBtn && cardCover) {
+    openCardBtn.addEventListener("click", () => {
+      setTimeout(() => {
+        cardCover.classList.add("hidden");
+      }, 600);
+    });
+  }
 });
 
 document.onreadystatechange = function () {
   if (document.readyState === "complete") {
     setTimeout(() => {
       document.querySelector("#loading-overlay").style.display = "none";
-    }, 3000);
+    }, LOAD_TIME);
   }
 };
